62f3adc475b1eabf6649bafa6a32018f3b9414cc
anonymous <mateo.martinez.spc@icloud.com>
2023-12-03 23:25:41 -0600

    content

--- 013-array/array.c
+++ 013-array/array.c
@@ -3,8 +3,33 @@
 
 int main() {
 
-    
+    int LEN = 5;
+    int a[5];
 
+    printf("initalized a[5]\n");
+    for (int x = 0; x < LEN; x++)
+    {
+        printf("%d\n", a[x]);
+    }
+    printf("\n");
+
+
+
+    printf("set a[5]\n");
+    for (int x = 1; x <= 5; x++)
+    {
+        a[x - 1] = x;
+    }
+    printf("\n");
+
+
+
+    printf("looping a[5]\n");
+    int len = sizeof(a) / sizeof(a[0]);
+    for (int x = 0; x < len; x++) {
+        printf("%d\n", a[x]);
+    }
+    printf("\n");
 
     return 0;
 }
